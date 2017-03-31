e04bb89efe4e49b023d612791b2f3acaffdf9dc2
Ivan Bosnic <ivanbosnic@univille.br>
2017-03-30 21:38:23 -0300

    insertion sort

--- InsertionSort/src/estd/Sorter.java
+++ InsertionSort/src/estd/Sorter.java
@@ -3,14 +3,36 @@ package estd;
 public class Sorter {
 
 	public void sort(int[] a) {
-		int n = a.length;
-		for (int i = 1; i < n; i++) {
-			int cur = a[i];
+		for (int i = 1; i < a.length; i++) {
+			int aux = a[i];
 			int j = i - 1;
-			while ((j >= 0) && (a[j] > cur)) {
+			while ((j >= 0) && (a[j] > aux)) {
 				a[j + 1] = a[j--];
 			}
-			a[j + 1] = cur;
+			a[j + 1] = aux;
 		}
 	}
+	
+	public void sort(char[] a) {
+		for (int i = 1; i < a.length; i++) {
+			char aux = a[i];
+			int j = i - 1;
+			while ((j >= 0) && (a[j] > aux)) {
+				a[j + 1] = a[j--];
+			}
+			a[j + 1] = aux;
+		}
+	}
+	
+	
+	public void sort(String[] a) {
+		for (int i = 1; i < a.length; i++) {
+			String aux = a[i];
+			int j = i - 1;
+			while ((j >= 0) && (a[j] > aux)) {
+				a[j + 1] = a[j--];
+			}
+			a[j + 1] = aux;
+		}
+	}	
 }
--- InsertionSort/test/estd/SorterTest.java
+++ InsertionSort/test/estd/SorterTest.java
@@ -13,5 +13,24 @@ public class SorterTest {
 		int[] expected = { 1, 2, 3, 5 };
 		Assert.assertArrayEquals(expected, n);
 	}
+	
+	@Test
+	public void sortCharTest() {
+		Sorter sorter = new Sorter();
+		char[] n = { 'D','C','B','A'};
+		sorter.sort(n);
+		char[] expected = { 'A', 'B', 'C', 'D' };
+		Assert.assertArrayEquals(expected, n);
+	}	
+	
+	@Test
+	public void sortStringTest() {
+		Sorter sorter = new Sorter();
+		String[] n = { "Fabio", "Ana"};
+		sorter.sort(n);
+		String[] expected = { "Ana", "Fabio"};
+		Assert.assertArrayEquals(expected, n);
+	}	
+	
 
 }
