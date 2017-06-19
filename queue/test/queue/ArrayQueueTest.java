5f3e790be18671734142645031ecc895c096bddc
Ivan Bosnic <ivanbosnic@univille.br>
2017-06-19 20:58:13 -0300

    queue

--- queue/test/queue/ArrayQueueTest.java
+++ queue/test/queue/ArrayQueueTest.java
@@ -37,4 +37,33 @@ public class ArrayQueueTest {
 		
 	}
 
+	@Test
+	public void circularTest() {
+		ArrayQueue<Integer> queue = new ArrayQueue<>(7);
+
+		queue.enqueue(0);
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+		queue.enqueue(4);
+		queue.enqueue(5);
+		queue.enqueue(6);
+		
+		queue.dequeue();
+		queue.dequeue();
+		queue.dequeue();
+		queue.dequeue();
+		
+		queue.enqueue(7);
+		queue.enqueue(8);
+		queue.enqueue(9);
+		queue.enqueue(10);
+		
+		queue.dequeue();
+		queue.dequeue();
+		queue.dequeue();
+		queue.dequeue();
+
+	}
+	
 }
